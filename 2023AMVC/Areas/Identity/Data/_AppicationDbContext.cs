3cbbf6d673af5980b9fd788cfa2d653a4c673e08
LENOVO <LENOVO@DESKTOP-HQLQUOA>
2025-03-18 11:03:36 +0545

    Add project files.

---
+++ 2023AMVC/Areas/Identity/Data/_AppicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2023AMVC.Data;
+
+public class _AppicationDbContext : IdentityDbContext<IdentityUser>
+{
+    public _AppicationDbContext(DbContextOptions<_AppicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
